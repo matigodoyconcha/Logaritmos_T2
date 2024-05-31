63f578dff150672bc39feadee54158727d9f3cc6
Claudio Morel <claudio.morel@ug.uchile.cl>
2024-05-31 02:09:08 -0400

    Correccion de metodo Pop de la clase cola fibonacci

--- main.cpp
+++ main.cpp
@@ -3,21 +3,15 @@
 #include "Dijkstra.cpp"
 
 int main(){
-    for (int i = 0; i < 10; i++){
+    for (int i = 0; i < 50; i++){
         Estructura *cola = new cola_fibonacci();
-        vector<vector<NodoDist>> graph = graphGen(14, 16);
+        vector<vector<NodoDist>> graph = graphGen(10, 16);
         cout << "Grafo generado" << endl;
-        tuple<vector<double>,vector<ull>> res = Dijkstra(pow(2,14), graph, *cola, 0);
+        tuple<vector<double>,vector<ull>> res = Dijkstra(pow(2,10), graph, *cola, 0);
         cout << "Dijkstra ejecutado" << endl;
         vector<double> distancias = get<0>(res);
         vector<ull> previos = get<1>(res);
         delete cola;
     }
-/*     for (int i = 0; i < distancias.size(); i++){
-        cout << "Distancia al nodo " << i << ": " << distancias[i] << endl;
-    }
-    for (int i = 0; i < previos.size(); i++){
-        cout << "Nodo previo al nodo " << i << ": " << previos[i] << endl;
-    } */
     return 0;
 };
